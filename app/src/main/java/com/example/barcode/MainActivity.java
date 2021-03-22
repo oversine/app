38380d9460b627ba322940b9b41fc625ed22e6bd
ikjun1117 <jij1117@naver.com>
2021-03-22 09:09:40 +0900

    inital commit

--- app/src/main/java/com/example/barcode/MainActivity.java
+++ app/src/main/java/com/example/barcode/MainActivity.java
@@ -21,9 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /////////////////////////////////////
-
-        //////////////////////////////////////
         mBottomNV = findViewById(R.id.navigation);
         mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
             @Override
